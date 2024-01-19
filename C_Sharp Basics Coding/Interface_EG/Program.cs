0943619dc0b2d13f813a52e3c1903adfe40a44a7
Nikhita Palla <nikhita_palla@epam.com>
2024-01-19 16:04:23 +0530

    Added Practised codes and learnings

---
+++ C_Sharp Basics Coding/Interface_EG/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+// Demonstrating Interfaces
+
+// Define two interfaces
+// multiple inheritance
+public interface IDisplayable
+{
+    void Log();
+}
+
+public interface ILoggable
+{
+    void Log();
+}
+
+public class MyClass : IDisplayable, ILoggable
+{
+    void IDisplayable.Log()
+    {
+        Console.WriteLine("Displaying...");
+    }
+
+    void ILoggable.Log()
+    {
+        Console.WriteLine("Logging...");
+    }
+}
+
+namespace Interface_EG
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            // Create an instance of MyClass
+            MyClass myObject = new MyClass();
+
+            // Use methods from both interfaces explicit implementation using downcasting
+            ((IDisplayable)myObject).Log();
+            ((ILoggable)myObject).Log();
+        }
+    }
+}
+
+
+
+
